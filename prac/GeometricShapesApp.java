005c45de3172da26dae7d4c1a55548c4c96c5ef6
shivambu108 <156665916+shivambu108@users.noreply.github.com>
2024-11-14 01:52:12 +0530

    prac

--- prac/GeometricShapesApp.java
+++ prac/GeometricShapesApp.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
 // Point2D Class
-class Point2D_23bcs123 {
+class Point2D {
     private int x, y;
-    public Point2D_23bcs123() { this.x = this.y = 0; }
-    public Point2D_23bcs123(int x, int y) { this.x = x; this.y = y; }
+    public Point2D() { this.x = this.y = 0; }
+    public Point2D(int x, int y) { this.x = x; this.y = y; }
     public int getX() { return x; }
     public int getY() { return y; }
-    public double calcDistance(Point2D_23bcs123 other) {
+    public double calcDistance(Point2D other) {
         return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
     }
 }
 
 // Line Class
-class Line_23bcs123 {
-    private Point2D_23bcs123 start, end;
-    public Line_23bcs123(Point2D_23bcs123 start, Point2D_23bcs123 end) { this.start = start; this.end = end; }
-    public Point2D_23bcs123 getStart() { return start; }
-    public Point2D_23bcs123 getEnd() { return end; }
+class Line {
+    private Point2D start, end;
+    public Line(Point2D start, Point2D end) { this.start = start; this.end = end; }
+    public Point2D getStart() { return start; }
+    public Point2D getEnd() { return end; }
     public double length() { return start.calcDistance(end); }
 }
 
 // Polygon Class
-abstract class Polygon_23bcs123 extends Line_23bcs123 {
-    protected List<Point2D_23bcs123> vertices;
-    public Polygon_23bcs123(List<Point2D_23bcs123> vertices) { super(vertices.get(0), vertices.get(1)); this.vertices = vertices; }
+abstract class Polygon extends Line {
+    protected List<Point2D> vertices;
+    public Polygon(List<Point2D> vertices) { super(vertices.get(0), vertices.get(1)); this.vertices = vertices; }
     public double perimeter() {
         double p = 0;
         for (int i = 0; i < vertices.size(); i++)
@@ -35,7 +35,7 @@ abstract class Polygon_23bcs123 extends Line_23bcs123 {
         int n = vertices.size(); if (n < 3) return false;
         boolean sign = false;
         for (int i = 0; i < n; i++) {
-            Point2D_23bcs123 p1 = vertices.get(i), p2 = vertices.get((i + 1) % n), p3 = vertices.get((i + 2) % n);
+            Point2D p1 = vertices.get(i), p2 = vertices.get((i + 1) % n), p3 = vertices.get((i + 2) % n);
             double crossProduct = (p2.getX() - p1.getX()) * (p3.getY() - p2.getY()) - (p2.getY() - p1.getY()) * (p3.getX() - p2.getX());
             if (i == 0) sign = crossProduct > 0;
             else if (sign != (crossProduct > 0)) return false;
@@ -46,24 +46,24 @@ abstract class Polygon_23bcs123 extends Line_23bcs123 {
 }
 
 // Circle Class
-class Circle_23bcs123 extends Polygon_23bcs123 {
+class Circle extends Polygon {
     private double radius;
-    public Circle_23bcs123(Point2D_23bcs123 center, double radius) { super(List.of(center, center)); this.radius = radius; }
+    public Circle(Point2D center, double radius) { super(List.of(center, center)); this.radius = radius; }
     @Override public double area() { return Math.PI * Math.pow(radius, 2); }
     @Override public double perimeter() { return 2 * Math.PI * radius; }
     @Override public boolean isConvex() { return true; }
 }
 
 // Square Class
-class Square_23bcs123 extends Polygon_23bcs123 {
-    public Square_23bcs123(List<Point2D_23bcs123> vertices) { super(vertices); }
+class Square extends Polygon {
+    public Square(List<Point2D> vertices) { super(vertices); }
     @Override public double area() { return Math.pow(vertices.get(0).calcDistance(vertices.get(1)), 2); }
     @Override public double perimeter() { return 4 * vertices.get(0).calcDistance(vertices.get(1)); }
 }
 
 // Rectangle Class
-class Rectangle_23bcs123 extends Polygon_23bcs123 {
-    public Rectangle_23bcs123(List<Point2D_23bcs123> vertices) { super(vertices); }
+class Rectangle extends Polygon {
+    public Rectangle(List<Point2D> vertices) { super(vertices); }
     @Override public double area() {
         double length = vertices.get(0).calcDistance(vertices.get(1)), width = vertices.get(1).calcDistance(vertices.get(2));
         return length * width;
@@ -75,7 +75,7 @@ class Rectangle_23bcs123 extends Polygon_23bcs123 {
 }
 
 // Main App
-public class GeometricShapesApp_23bcs123 {
+public class GeometricShapesApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Do you want to create a circle (y/n)? ");
@@ -87,25 +87,25 @@ public class GeometricShapesApp_23bcs123 {
             int centerY = scanner.nextInt();
             System.out.print("Enter the radius of the circle: ");
             double radius = scanner.nextDouble();
-            Circle_23bcs123 circle = new Circle_23bcs123(new Point2D_23bcs123(centerX, centerY), radius);
+            Circle circle = new Circle(new Point2D(centerX, centerY), radius);
             System.out.println("Area of the circle: " + circle.area());
             System.out.println("Perimeter (Circumference) of the circle: " + circle.perimeter());
         } else {
             System.out.print("Enter the number of vertices for the polygon: ");
             int numVertices = scanner.nextInt();
-            List<Point2D_23bcs123> vertices = new ArrayList<>();
+            List<Point2D> vertices = new ArrayList<>();
             for (int i = 0; i < numVertices; i++) {
                 System.out.print("Enter x-coordinate of vertex " + (i + 1) + ": ");
                 int x = scanner.nextInt();
                 System.out.print("Enter y-coordinate of vertex " + (i + 1) + ": ");
                 int y = scanner.nextInt();
-                vertices.add(new Point2D_23bcs123(x, y));
+                vertices.add(new Point2D(x, y));
             }
-            Polygon_23bcs123 polygon;
+            Polygon polygon;
             if (numVertices == 4) {
                 System.out.print("Is it a square (y/n)? ");
-                polygon = scanner.next().equalsIgnoreCase("y") ? new Square_23bcs123(vertices) : new Rectangle_23bcs123(vertices);
-            } else polygon = new Polygon_23bcs123(vertices) { @Override public double area() { return 0; }};
+                polygon = scanner.next().equalsIgnoreCase("y") ? new Square(vertices) : new Rectangle(vertices);
+            } else polygon = new Polygon(vertices) { @Override public double area() { return 0; }};
             System.out.println("Area of the polygon: " + polygon.area());
             System.out.println("Perimeter of the polygon: " + polygon.perimeter());
             System.out.println("Is the polygon convex? " + polygon.isConvex());
